04d688c4754c7164762c0a1af0cabfc4c2653211
Brian Werner <brianmw199@gmail.com>
2023-11-10 01:04:41 -0600

    Created the get route and tested on potman

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,23 @@
 console.log('JS is sourced!');
 
+// Create a GET function to get data from the server(call the render function in .then)
+function getTask() {
+  
+  // axios call to the server to get todos
+  axios({
+    method: 'GET',
+    url: '/todos'
+  }).then((response) => {
+    console.log('In the then function');
+  })
+
+};// End of getTask
+
 // Create a submit function that has a POST route to send a request and recieve a response.(aka submit button)
 // call the get function in .then
-
-// Create a GET function to get data from the server(call the render function in .then)
+function submitTask(event) {
+    
+};
 
 // Create a update function that is connected to a PUT route to update the database
 
--- server/routes/todos.router.js.js
+++ server/routes/todos.router.js.js
@@ -2,9 +2,20 @@ const router = require('express').Router();
 const pool = require('../modules/pool');
 
 // GET ROUTE 
-// router.get('/')
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "todos" ORDER BY "id";'
+    pool.query(queryText)
+    .then((dbResult) => {
+        console.log('GET /todos', dbResult.rows)
+        res.send(dbResult.rows)
+    })
+    .catch((dbError) => {
+        console.log('error with getting todo:', dbError)
+        res.sendStatus(500)
+    })
+});
 
-// POST route with "task", "location", "complete" ? so the database can call the boolean
+// POST route to post new task
 
 // PUT route
 
