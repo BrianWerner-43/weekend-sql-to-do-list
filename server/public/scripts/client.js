8af2f4a744aaf42caa276a1e0bf8ca4490a0e342
Brian Werner <brianmw199@gmail.com>
2023-11-11 23:15:02 -0600

    Updated the post route on the server and client and targeted the data test ids in the render

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,7 @@
 console.log('JS is sourced!');
 
+getTask();
+
 // Create a GET function to get data from the server(call the render function in .then)
 function getTask() {
   
@@ -8,7 +10,10 @@ function getTask() {
     method: 'GET',
     url: '/todos'
   }).then((response) => {
-    console.log('In the then function');
+    console.log('GET /todos:', response.data);
+    renderTask(response.data);
+  }).catch((error) => {
+    console.log('error with GET:', error);
   })
 
 };// End of getTask
@@ -16,11 +21,74 @@ function getTask() {
 // Create a submit function that has a POST route to send a request and recieve a response.(aka submit button)
 // call the get function in .then
 function submitTask(event) {
-    
-};
+    event.preventDefault();
+    let taskToDo = document.getElementById('todoItem').value;
+    // let completeTask = document.getElementById('isComplete').value;
+    document.getElementById('todoItem').value = '';
+    // document.getElementById('isComplete').value = '';
+
+    axios({
+      method: 'POST',
+      url: '/todos',
+      data:
+      {
+        taskToDo: taskToDo,
+        // completeTask: completeTask
+        
+      }
+    }).then(function(response) {
+      console.log('POST /todos is getting a response:');
+      getTask();
+    }).catch(function(error) {
+      console.log('error in POST:', error);
+    })
+};// End of submitTask
+
+// Render function to append the taksk table to the DOM
+function renderTask(tasks) {
+  let taskList = document.getElementById('taskList');
+  taskList.innerHTML = '';
+  console.log('In renderTask');
+  
+
+  for(let task of tasks) {
+    console.log(task.text);
+    taskList.innerHTML +=
+    `<tr data-todoid = "${task.id}">
+        <td data-testid="toDoItem" class= ${task.isComplete ? "completed" : "not-complete"}>
+
+        <button data-testid="completeButton"
+        class="completed" onclick="updateTask(event)">Complete</button>${task.text}
+        <button data-testid="deleteButton" onclick="deleteTask(event)">Delete</button></td>
+        <td>${task.isComplete}</td>
+      </tr>
+        
+      
+    `
+    // if(task.isComplete) {
+    //   document.getElementById(task.id).classList.add("completed");
+    // }
+    // console.log('Did class change?');
+  }
+}
+
 
 // Create a update function that is connected to a PUT route to update the database
+function updateTask(event) {
+  let taskId = event.target.closet("tr").getAttribute("data-taskid");
+  console.log('In updateTask:', taskId);
+  axios({
+    method: 'PUT',
+    url: `todos/${taskId}`
+  }).then((response) => {
+    console.log('Task by id', response);
+    getTask();
+  }).catch((error) => {
+    console.log('error with PUT:', error)
+  })
+};
+
 
 // Delete function that deletes the task from the DOM and database
 
-// Render function to append the taksk table to the DOM
+
---
+++ server/routes/todos.router.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+const pool = require('../modules/pool');
+
+// GET ROUTE 
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "todos" ORDER BY "id";'
+    pool.query(queryText)
+    .then((dbResult) => {
+        console.log('GET /todos', dbResult.rows)
+        res.send(dbResult.rows)
+    })
+    .catch((dbError) => {
+        console.log('error with getting todo:', dbError)
+        res.sendStatus(500)
+    })
+});
+
+// POST route to post new task
+router.post('/', (req, res) => {
+    
+    console.log('In post route:', req.body);
+    
+    let queryText = 
+    `INSERT INTO "todos"
+    ("text")
+    VALUES
+    ($1)
+    `
+    const sqlValues = [
+        req.body.taskToDo
+    ]
+
+    pool.query(queryText, sqlValues)
+    .then((dbResult) => {
+        res.sendStatus(201)
+    })
+    .catch((dbError) => {
+        console.log('POST /todos query faild:', dbError)
+    })
+});
+
+// PUT route
+router.put('/:id', (req, res) => {
+    let idToUpdate = req.params.id;
+
+    queryText = `UPDATE "todos" SET "isComplete" = TRUE WHERE "id"=$1`;
+
+    const sqlValues = [idToUpdate];
+
+    pool.query(queryText, sqlValues)
+    .then((dbResult) => {
+        console.log('Task updated:');
+        res.sendStatus(200);
+    })
+    .catch((dbError) => {
+        console.log('PUT /todos:id failed:', dbError);
+        res.sendStatus(500);
+    })
+    
+
+})
+
+// Delete route
+
+
+module.exports = router;
