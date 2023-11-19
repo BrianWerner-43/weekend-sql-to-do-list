628f971b88eebc3d6f9a693400f2b28034b7d078
Brian Werner <brianmw199@gmail.com>
2023-11-19 08:57:51 -0600

    Update pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,16 +1,33 @@
-const pg = require('pg')
-
+const pg = require('pg');
 let databaseName = 'weekend-to-do-app'
+let pool;
 
-if (process.env.NODE_ENV === 'test') {
-  databaseName = 'prime_testing'
+// When our app is deployed to the internet 
+// we'll use the DATABASE_URL environment variable
+// to set the connection info: web address, username/password, db name
+// eg: 
+//  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
+if (process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
 }
-
-const pool = new pg.Pool({
+else {
+  pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
     database: databaseName,
     allowExitOnIdle: true 
 })
+
+}
+
+
+
+
+
 
 module.exports = pool
